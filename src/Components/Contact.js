1dc77dd887b6f069aabce459fafd73bda6e3f7c5
Atindra Mohan das <atindramohandas353@gmail.com>
2023-08-18 09:14:45 +0530

    complete contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,9 +2,33 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h2>Contact Us</h2>
-      <p>You can reach us at contact@example.com</p>
+    <div className="contact-container">
+      <h2 className="contact-heading">Contact Us</h2>
+      <p className="contact-description">
+        Have questions or need assistance? Reach out to us anytime.
+      </p>
+      <div className="contact-info">
+        <div className="contact-details">
+          <h3>Customer Support</h3>
+          <p>Email: <a href="mailto:contact@loanreminderapp.com">contact@loanreminderapp.com</a></p>
+          <p>Phone: <a href="tel:+1234567890">+123-456-7890</a></p>
+        </div>
+        <div className="contact-form">
+          <h3>Send us a Message</h3>
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required />
+
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2023 Loan Reminder App. All rights reserved.</p>
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} Loan Reminder App. All rights reserved.</p>
       </div>
     </footer>
   );
