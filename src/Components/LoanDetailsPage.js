de908c37d35afb734ac6f1c5638756b8d003d69a
Atindra Mohan das <atindramohandas353@gmail.com>
2023-08-19 16:11:49 +0530

    Maximum change and style into  LoanDetailesPage

--- src/Components/LoanDetailsPage.js
+++ src/Components/LoanDetailsPage.js
@@ -1,31 +1,141 @@
 import React, { useState } from "react";
 
 const LoanDetailsPage = () => {
+  const [userName, setUserName] = useState("");
   const [loanAmount, setLoanAmount] = useState("");
-  const [interestRate, setInterestRate] = useState("");
-  // Add more state variables for other loan details
+  const [loanType, setLoanType] = useState("student");
+  const [interestRate, setInterestRate] = useState("4%");
+  const [paymentFrequency, setPaymentFrequency] = useState("daily");
+  const [repaymentAmount, setRepaymentAmount] = useState(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle saving the loan details here
+  const handleLoanAmountChange = (event) => {
+    setLoanAmount(event.target.value);
+  };
+
+  const handleLoanTypeChange = (event) => {
+    setLoanType(event.target.value);
+  };
+
+  const handleInterestRateChange = (event) => {
+    setInterestRate(event.target.value);
+  };
+
+  const handlePaymentFrequencyChange = (event) => {
+    setPaymentFrequency(event.target.value);
+  };
+
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
+
+  const calculateRepaymentAmount = () => {
+    const calculatedAmount = loanAmount * 1.04; // Adding 4% interest
+    setRepaymentAmount(calculatedAmount.toFixed(2)); // Fixed to 2 decimal places
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    calculateRepaymentAmount();
   };
 
   return (
-    <div className="loan-details-page">
-      <h1>Enter Your Loan Details</h1>
+    <div className="loan-details-container">
+    <div className="colorful-box">
+      <h1>Loan Details</h1>
       <form onSubmit={handleSubmit}>
-        <label>Loan Amount:</label>
-        <input
-          type="number"
-          value={loanAmount}
-          onChange={(e) => setLoanAmount(e.target.value)}
-        />
-        {/* Add more input fields for other loan details */}
-        <button type="submit">Save Loan Details</button>
+        <div className="input-container">
+          <label htmlFor="userName">User Name:</label>
+          <input
+            className="input"
+            type="text"
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="loanAmount">Loan Amount:</label>
+          <input
+            className="input"
+            type="number"
+            value={loanAmount}
+            onChange={handleLoanAmountChange}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="loanType">Loan Type:</label>
+          <select
+            className="input"
+            value={loanType}
+            onChange={handleLoanTypeChange}
+          >
+            <option value="student">Student Loan</option>
+            <option value="business">Business Loan</option>
+            <option value="home">Home Loan</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <label htmlFor="interestRate">Interest Rate:</label>
+          <input
+            className="input"
+            type="number"
+            value={interestRate}
+            onChange={handleInterestRateChange}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="paymentFrequency">Payment Frequency:</label>
+          <select
+            className="input"
+            value={paymentFrequency}
+            onChange={handlePaymentFrequencyChange}
+          >
+            <option value="daily">Daily</option>
+            <option value="weekly">Weekly</option>
+            <option value="monthly">Monthly</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <label htmlFor="startDate">Start Date:</label>
+          <input
+            className="input"
+            type="date"
+            value={startDate}
+            onChange={handleStartDateChange}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="endDate">End Date:</label>
+          <input
+            className="input"
+            type="date"
+            value={endDate}
+            onChange={handleEndDateChange}
+            required
+          />
+        </div>
+        <button type="submit" className="calculate-button">
+          Calculate
+        </button>
       </form>
+      {repaymentAmount !== null && (
+        <div className="repayment-amount">
+          <p>Calculated Repayment Amount: ${repaymentAmount}</p>
+        </div>
+      )}
+    </div>
     </div>
   );
 };
+
 
 export default LoanDetailsPage;
